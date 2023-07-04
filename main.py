17a485d98b0103529e05f26876df87b787077d76
smartshoper <harshharia1234@gmail.com>
2023-07-04 23:53:24 +0530

    BaseModel added. FAST API using pydantic figures out how to pass values and parse on its own.

--- main.py
+++ main.py
@@ -3,25 +3,32 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+from enum import Enum  
 app = FastAPI()
 
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: Union[bool, None] = None
+
+class Category(Enum):
+    TOOLS="tools"
+    CONSUMABLES="consumables"
 
 class Item(BaseModel):
     name: str
     price: float
-    is_offer: Union[bool, None] = None
+    count: int
+    id: int
+    Category: Category
 
+items = {
+    0: Item(name="Hammer", price=9.99, count=20, id=0, Category=Category.TOOLS),
+    1: Item(name="Nails", price=5.99, count=100, id=1, Category=Category.CONSUMABLES),  
+    2: Item(name="Piers", price=9.99, count=20, id=2, Category=Category.TOOLS),
+}
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
-
-
-@app.put("/items/{item_id}")
-def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+@app.get('/')
+def index() -> dict[str, dict[int, Item]]:
+    return {'items': items}
