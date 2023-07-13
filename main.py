3c71fa3cc530c89c1c029c83b466af7b75aa7c0d
smartshoper <harshharia1234@gmail.com>
2023-07-13 23:09:56 +0530

    find_one, update and delete function added.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from utility.config import conn
+from bson import ObjectId
 
 from models.studentDetails import StudentDetails
 
@@ -12,9 +13,10 @@ def index():
 
 @app.get('/fetchAll')
 async def fetchAll():
-    data = conn.LibraryManagementSystem.students.find({}, {'_id': 0})
+    data = conn.LibraryManagementSystem.students.find({})
     studentData = []
     for item in data:
+        item['_id'] = str(item['_id'])
         studentData.append(item)
     print(studentData)
     print(type(studentData))
@@ -25,3 +27,21 @@ async def addStudent(student: StudentDetails):
     response = conn.LibraryManagementSystem.students.insert_one(student.dict())
     print(response)
     return {'message': 'Student added successfully!'}
+
+@app.get('/fetchStudent/{id}')
+async def fetchStudent(id: str):
+    data = conn.LibraryManagementSystem.students.find_one({'_id': ObjectId(id)})
+    data['_id'] = str(data['_id'])
+    return data
+
+@app.put('/updateStudent/{id}')
+async def updateStudent(id: str, student: StudentDetails):
+    response = conn.LibraryManagementSystem.students.update_one({'_id': ObjectId(id)}, {'$set': student.dict()})
+    print(response)
+    return {'message': f'Student {id} updated successfully!'}
+
+@app.delete('/deleteStudent/{id}')
+async def deleteStudent(id: str):
+    response = conn.LibraryManagementSystem.students.delete_one({'_id': ObjectId(id)})
+    print(response)
+    return {'message': f'Student {id} deleted successfully!'}
